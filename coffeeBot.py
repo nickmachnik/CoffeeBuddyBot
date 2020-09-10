9346117bcb4b15e1aab33a0191b2ccfa0f5883bf
Nick Machnik <nick.machnik@gmail.com>
2020-09-10 12:34:17 +0200

    Move code to class

--- coffeeBot.py
+++ coffeeBot.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-# import smtplib
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import json
 
 
@@ -17,14 +17,24 @@ def main():
         json.dump(config, fout, indent=2, sort_keys=True)
 
 
-def make_pairs(i, people):
-    hs = len(people) // 2
-    fixed, moving = people[-1], people[:-1]
-    current_rotation = moving[i:] + moving[:i]
-    side_a, side_b = current_rotation[:hs], current_rotation[hs:]
-    side_b = side_b[::-1] + [fixed]
-    for a, b in zip(side_a, side_b):
-        yield(a, b)
+class CoffeeBot:
+    def __init__(self, config):
+        self.recipients = config["recipients"]
+        self.sender_account = config["sender_account"]
+        self.sender_password = config["sender_password"]
+        self.sender_username = config["sender_username"]
+        self.smtp_port = config["smtp_port"]
+        self.smtp_server = config["smtp_server"]
+        self.round = config["round"]
+
+    def _make_pairs(self):
+        hs = len(self.recipients) // 2
+        fixed, moving = self.recipients[-1], self.recipients[:-1]
+        current_rotation = moving[self.round:] + moving[:self.round]
+        side_a, side_b = current_rotation[:hs], current_rotation[hs:]
+        side_b = side_b[::-1] + [fixed]
+        for a, b in zip(side_a, side_b):
+            yield(a, b)
 
 
 if __name__ == '__main__':
