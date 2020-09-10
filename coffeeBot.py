cd48b14526375ad0aa1dcfb8303c4a4e233654f3
Nick Machnik <nick.machnik@gmail.com>
2020-09-10 12:36:33 +0200

    Add draft of mail sending method

--- coffeeBot.py
+++ coffeeBot.py
@@ -7,18 +7,22 @@ import json
 
 
 def main():
+
     with open("./config.json", 'r') as fin:
         config = json.load(fin)
-    config["round"] = config["round"] % (len(config["email_recepients"]) - 1)
-    pairs_iter = make_pairs(
-        config["round"], config["email_recepients"])
-    config["round"] += 1
-    with open("./config.json", 'w') as fout:
-        json.dump(config, fout, indent=2, sort_keys=True)
+    bot = CoffeeBot(config)
+    bot._send_mails()
+    # config["round"] = config["round"] % (len(config["email_recepients"]) - 1)
+    # pairs_iter = make_pairs(
+    #     config["round"], config["email_recepients"])
+    # config["round"] += 1
+    # with open("./config.json", 'w') as fout:
+    #     json.dump(config, fout, indent=2, sort_keys=True)
 
 
 class CoffeeBot:
     def __init__(self, config):
+        self.subject = config["subject"]
         self.recipients = config["recipients"]
         self.sender_account = config["sender_account"]
         self.sender_password = config["sender_password"]
@@ -26,6 +30,26 @@ class CoffeeBot:
         self.smtp_port = config["smtp_port"]
         self.smtp_server = config["smtp_server"]
         self.round = config["round"]
+
+    def _send_mails(self):
+        #login to email server
+        server = smtplib.SMTP(self.smtp_server,self.smtp_port)
+        server.starttls()
+        server.login(self.sender_username, self.sender_password)
+        for a, b in self._make_pairs():
+            if a == "BREAK":
+                continue
+            elif b == "BREAK":
+                continue
+            else:
+                message = MIMEMultipart('alternative')
+                message['From'] = self.sender_account
+                message['To'] = recipient
+                message['Subject'] = self.subject
+                message.attach(MIMEText(self.body, 'html'))
+                text = message.as_string()
+                server.sendmail(self.sender_account,recipient,text)#All emails sent, log out.
+        server.quit()
 
     def _make_pairs(self):
         hs = len(self.recipients) // 2
